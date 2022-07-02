e843b2c9fea191ab4e88be062a7fb694008fc465
conrad030 <conrad.felgentreff@web.de>
2022-07-02 15:18:23 +0200

    Initial Commit

---
+++ TestClient/TestClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestClientApp.swift
+//  TestClient
+//
+//  Created by Conrad Felgentreff on 02.07.22.
+//
+
+import SwiftUI
+
+@main
+struct TestClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
